ee35c76675235815bf8aa0fdc8ef1fa6dadaf7a0
MythosMa <mythosmayuan@gmail.com>
2023-04-22 12:15:15 +0800

    revert start scene

--- assets/Script/GameScript/GameController.js
+++ assets/Script/GameScript/GameController.js
@@ -78,7 +78,7 @@ export default class GameController extends cc.Component {
     onLoad(){
         cc.director.getPhysicsManager().enabled = true;
         this.maxUnit = UnitInfo.getUnitCount();
-        this.currentUnitIndex = 7;
+        this.currentUnitIndex = 0;
         this.blocks = [];
         this.blockPool = new cc.NodePool();
         this.eatBlocks = [];
